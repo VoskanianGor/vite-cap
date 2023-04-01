4f26ff74496d856dd5116acb78eebdbfc795f8fc
VoskanianGor <vosckaniangor@gmail.com>
2023-04-01 15:54:00 +0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vite.cap',
+  appName: 'vite-cap',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
